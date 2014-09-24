42b4a1168e509a043b775c8305854cf03edbb635
NarekSedrakyan90 <beliyprizrak@mail.ru>
2014-09-24 16:42:33 +0400

    Added new version

---
+++ CityMobilDriver/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  CityMobilDriver
+//
+//  Created by Intern on 9/24/14.
+//  Copyright (c) 2014 Davit Baghdagyulyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+- (IBAction)actionBack:(UIButton *)sender;
+
+@end
