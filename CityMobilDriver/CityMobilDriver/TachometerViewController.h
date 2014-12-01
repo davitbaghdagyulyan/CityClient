41b4e4a22e7d3524c937e1fe673157fd15480c74
karen karapetyan <karen250595@mail.ru>
2014-12-01 14:48:19 +0400

    karen comit

--- CityMobilDriver/CityMobilDriver/TachometerViewController.h
+++ CityMobilDriver/CityMobilDriver/TachometerViewController.h
@@ -10,4 +10,31 @@
 
 @interface TachometerViewController : UIViewController
 
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *tachoElements;
+
+@property (weak, nonatomic) IBOutlet UIView *navigationView;
+@property (weak, nonatomic) IBOutlet UIView *bgView;
+
+@property (weak, nonatomic) IBOutlet UILabel *distance;
+
+
+
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *elements;
+
+- (IBAction)elementAction:(id)sender;
+
+
+
+//@property(nonatomic,strong)NSString * name;
+
+@property(nonatomic,strong)NSString* collDate;
+@property(nonatomic,strong)NSString* shortname;
+@property(nonatomic,strong)NSString* collMetroName;
+@property(nonatomic,strong)NSString* CollComment;
+@property(nonatomic,strong)NSString* deliveryMetroName;
+@property(nonatomic,strong)NSString* deliveryComment;
+@property(nonatomic,strong)NSString* ourComment;
+
+
+///name price from services
 @end
