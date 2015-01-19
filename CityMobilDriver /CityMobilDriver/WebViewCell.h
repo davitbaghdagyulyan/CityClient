334f015fac31480512df88b3a7ef9fc2e9427a3c
karen karapetyan <karen250595@mail.ru>
2015-01-19 12:36:35 +0400

    Karen bug fix

---
+++ CityMobilDriver /CityMobilDriver/WebViewCell.h
@@ -0,0 +1,14 @@
+//
+//  WebViewCell.h
+//  CityMobilDriver
+//
+//  Created by Intern on 1/12/15.
+//  Copyright (c) 2015 Davit Baghdagyulyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
